73d716f08b13159ad62b4ed8c9d0d21f1685c275
amralsaleeh <3mr.alsaelh@gmail.com>
2023-02-05 15:37:26 +0300

    Main

--- resources/views/livewire/diet.blade.php
+++ resources/views/livewire/diet.blade.php
@@ -30,33 +30,87 @@
             </div>
         </div>
         <div class="content-body">
+            <div class="row" id="basic-table">
+                <div class="col-12">
+                    <div class="card">
 
-            @php
-                $diet = ['1','2','3','4'];
-                $count = 4;
-            @endphp
+                        <div class="card-header d-flex justify-content-between align-items-center" style="margin-bottom: -15px;">
+                            <h4 class="card-title mb-sm-0 mb-1">الوجبات</h4>
+                            <a href="https://wa.me/message/POU44V4CNULGN1" style="color: gray">هل لديك سؤال أو استفسار عن وجبة معينة, تواصل معي؟</a>
+                        </div>
 
-            <div class="col-12">
-                <div class="card">
-                    <div class="col-12">
-                        @foreach ( $diet as $d )
-                        <div class="card">
-                            <div
-                                class="card-header d-flex justify-content-between align-items-center">
-                                <h4 class="card-title mb-sm-0 mb-1" style="color: #f8991d">الفطور</h4>
-                            </div>
-                            <div class="card-body" style="text-align: center;font-size: 18px;">
-                                <p>ديتوكس (ليمون + كاسة ماء) - فيتامين ب-12 (200مكغ)</p>
-                                <p>بيض مسلوق (2) - جبنة (75غ) - محمص خبز شوفان (1ر) - أوميغا-3 (1000مغ)</p>
+                        <div class="card-body">
+                            <div class="accordion accordion-margin" id="accordionExample">
+                                <div class="card accordion-item">
+                                    <h2 class="accordion-header" id="headingOne">
+                                        <button
+                                        class="accordion-button collapsed"
+                                        data-bs-toggle="collapse"
+                                        role="button"
+                                        data-bs-target="#collapseOne"
+                                        aria-expanded="false"
+                                        aria-controls="collapseOne"
+                                        >
+                                        <h4 class="card-title mb-sm-0 mb-1" style="color: #f8991d ;margin-bottom: 0rem !important;">الفطور</h4>
+                                        </button>
+                                    </h2>
+
+                                    <div
+                                        id="collapseOne"
+                                        class="accordion-collapse collapse"
+                                        aria-labelledby="headingOne"
+                                        data-bs-parent="#accordionExample"
+                                    >
+                                        <div class="accordion-body">
+                                        <div
+                                        class="card-header d-flex justify-content-between align-items-center">
+                                        </div>
+                                        <div class="card-body" style="text-align: center;font-size: 18px;">
+                                            <p>ديتوكس (ليمون + كاسة ماء) - فيتامين ب-12 (200مكغ)</p>
+                                            <p>بيض مسلوق (2) - جبنة (75غ) - محمص خبز شوفان (1ر) - أوميغا-3 (1000مغ)</p>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card accordion-item">
+                                    <h2 class="accordion-header" id="headingTwo">
+                                        <button
+                                        class="accordion-button collapsed"
+                                        data-bs-toggle="collapse"
+                                        role="button"
+                                        data-bs-target="#collapseTwo"
+                                        aria-expanded="false"
+                                        aria-controls="collapseTwo"
+                                        >
+                                        <h4 class="card-title mb-sm-0 mb-1" style="color: #f8991d ;margin-bottom: 0rem !important;">الغداء</h4>
+                                        </button>
+                                    </h2>
+
+                                    <div
+                                        id="collapseTwo"
+                                        class="accordion-collapse collapse"
+                                        aria-labelledby="headingTwo"
+                                        data-bs-parent="#accordionExample"
+                                    >
+                                        <div class="accordion-body">
+                                        <div
+                                        class="card-header d-flex justify-content-between align-items-center">
+                                        </div>
+                                        <div class="card-body" style="text-align: center;font-size: 18px;">
+                                            <p>ديتوكس (ليمون + كاسة ماء) - فيتامين ب-12 (200مكغ)</p>
+                                            <p>بيض مسلوق (2) - جبنة (75غ) - محمص خبز شوفان (1ر) - أوميغا-3 (1000مغ)</p>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        @if ($count > 1)
-                        <hr>
-                        @php
-                            $count--;
-                        @endphp
-                        @endif
-                        @endforeach
+                        <div class="card-body">
+                            <p class="card-text">
+                              الجدول السابق يوضح الوجبات الخاصة بك بالخطة الحالية بناء على حالتك الخاصة, يمكنك النقر على أسم الوجبة لمشاهدة التفاصيل الخاصة بها, في حال وجود أي وجبة غير مفهومة الرجاء <a href="https://wa.me/message/POU44V4CNULGN1">التواصل معي</a> بشكل مباشر
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/livewire/exercise.blade.php
+++ resources/views/livewire/exercise.blade.php
@@ -32,71 +32,183 @@
         <div class="content-body">
             <div class="row" id="basic-table">
                 <div class="col-12">
-                  <div class="card">
+                    <div class="card">
 
-                    <div class="table-responsive">
-                      <table class="table">
-                        <thead>
-                          <tr>
-                            <th style="border-radius: 0px 10px 0px 0px;">التمرين</th>
-                            <th>العدات</th>
-                            <th style="border-radius: 10px 0px 0px 0px;">الإيقاع</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                              <td>
-                                  <a href="#" class="fw-bold">DUMBBEL ROW</a>
-                              </td>
-                              <td>8 * 3</td>
-                              <td>1-2-1</td>
-                          </tr>
-                          <tr>
-                              <td>
-                                  <a href="#" class="fw-bold">CABLE PULL DOWN</a>
-                              </td>
-                              <td>12 * 3</td>
-                              <td>1-2-1</td>
-                            </tr>
-                            <tr>
-                              <td>
-                                  <a href="#" class="fw-bold">BICEPS CURL MACHINE</a>
-                              </td>
-                              <td>8 * 3</td>
-                              <td>1-2-1</td>
-                            </tr>
-                            <tr>
-                              <td>
-                                  <a href="#" class="fw-bold">BICEPS CABLE CURL</a>
-                              </td>
-                              <td>12 * 3</td>
-                              <td>1-2-1</td>
-                            </tr>
-                            <tr>
-                              <td>
-                                  <a href="#" class="fw-bold">SHRUG</a>
-                              </td>
-                              <td>8 * 3</td>
-                              <td>1-2-2</td>
-                            </tr>
-                            <tr>
-                              <td>
-                                  <a href="#" class="fw-bold">FACE PULL</a>
-                              </td>
-                              <td>12 * 3</td>
-                              <td>1-2-3</td>
-                            </tr>
-                        </tbody>
-                      </table>
+                        <div class="card-header d-flex justify-content-between align-items-center" style="margin-bottom: -15px;">
+                            <h4 class="card-title mb-sm-0 mb-1">أيام التدريب</h4>
+                            <a href="https://wa.me/message/POU44V4CNULGN1" style="color: gray">هل لديك سؤال أو استفسار عن تمرين معين, تواصل معي؟</a>
+                        </div>
+
+                        <div class="card-body">
+                            <div class="accordion accordion-margin" id="accordionExample">
+                                <div class="card accordion-item">
+                                    <h2 class="accordion-header" id="headingOne">
+                                        <button
+                                        class="accordion-button collapsed"
+                                        data-bs-toggle="collapse"
+                                        role="button"
+                                        data-bs-target="#collapseOne"
+                                        aria-expanded="false"
+                                        aria-controls="collapseOne"
+                                        >
+                                        <h4 class="card-title mb-sm-0 mb-1" style="color: #f8991d ;margin-bottom: 0rem !important;">اليوم الأول</h4>
+                                        </button>
+                                    </h2>
+
+                                    <div
+                                        id="collapseOne"
+                                        class="accordion-collapse collapse"
+                                        aria-labelledby="headingOne"
+                                        data-bs-parent="#accordionExample"
+                                    >
+                                        <div class="accordion-body">
+                                            <div class="table-responsive">
+                                                <table class="table">
+                                                <thead>
+                                                    <tr>
+                                                    <th style="border-radius: 0px 10px 0px 0px;">التمرين</th>
+                                                    <th>العدات</th>
+                                                    <th style="border-radius: 10px 0px 0px 0px;">الإيقاع</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>
+                                                            <a href="#" class="fw-bold">DUMBBEL ROW</a>
+                                                        </td>
+                                                        <td>8 * 3</td>
+                                                        <td>1-2-1</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <a href="#" class="fw-bold">CABLE PULL DOWN</a>
+                                                        </td>
+                                                        <td>12 * 3</td>
+                                                        <td>1-2-1</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <a href="#" class="fw-bold">BICEPS CURL MACHINE</a>
+                                                        </td>
+                                                        <td>8 * 3</td>
+                                                        <td>1-2-1</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <a href="#" class="fw-bold">BICEPS CABLE CURL</a>
+                                                        </td>
+                                                        <td>12 * 3</td>
+                                                        <td>1-2-1</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <a href="#" class="fw-bold">SHRUG</a>
+                                                        </td>
+                                                        <td>8 * 3</td>
+                                                        <td>1-2-2</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <a href="#" class="fw-bold">FACE PULL</a>
+                                                        </td>
+                                                        <td>12 * 3</td>
+                                                        <td>1-2-3</td>
+                                                    </tr>
+                                                </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card accordion-item">
+                                    <h2 class="accordion-header" id="headingTwo">
+                                        <button
+                                        class="accordion-button collapsed"
+                                        data-bs-toggle="collapse"
+                                        role="button"
+                                        data-bs-target="#collapseTwo"
+                                        aria-expanded="false"
+                                        aria-controls="collapseTwo"
+                                        >
+                                        <h4 class="card-title mb-sm-0 mb-1" style="color: #f8991d ;margin-bottom: 0rem !important;">اليوم الثاني</h4>
+                                        </button>
+                                    </h2>
+
+                                    <div
+                                        id="collapseTwo"
+                                        class="accordion-collapse collapse"
+                                        aria-labelledby="headingTwo"
+                                        data-bs-parent="#accordionExample"
+                                    >
+                                        <div class="accordion-body">
+                                            <div class="table-responsive">
+                                                <table class="table">
+                                                <thead>
+                                                    <tr>
+                                                    <th style="border-radius: 0px 10px 0px 0px;">التمرين</th>
+                                                    <th>العدات</th>
+                                                    <th style="border-radius: 10px 0px 0px 0px;">الإيقاع</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>
+                                                            <a href="#" class="fw-bold">DUMBBEL ROW</a>
+                                                        </td>
+                                                        <td>8 * 3</td>
+                                                        <td>1-2-1</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <a href="#" class="fw-bold">CABLE PULL DOWN</a>
+                                                        </td>
+                                                        <td>12 * 3</td>
+                                                        <td>1-2-1</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <a href="#" class="fw-bold">BICEPS CURL MACHINE</a>
+                                                        </td>
+                                                        <td>8 * 3</td>
+                                                        <td>1-2-1</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <a href="#" class="fw-bold">BICEPS CABLE CURL</a>
+                                                        </td>
+                                                        <td>12 * 3</td>
+                                                        <td>1-2-1</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <a href="#" class="fw-bold">SHRUG</a>
+                                                        </td>
+                                                        <td>8 * 3</td>
+                                                        <td>1-2-2</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <a href="#" class="fw-bold">FACE PULL</a>
+                                                        </td>
+                                                        <td>12 * 3</td>
+                                                        <td>1-2-3</td>
+                                                    </tr>
+                                                </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">
+                              الجدول السابق يوضح التمارين الخاصة بك بالخطة الحالية بناء على حالتك الخاصة, يمكنك النقر على أسم التمرين لمشاهدة الفيديو الخاص به, في حال وجود أي تمرين غير مفهوم الرجاء <a href="https://wa.me/message/POU44V4CNULGN1">التواصل معي</a> بشكل مباشر
+                            </p>
+                        </div>
+
                     </div>
-
-                    <div class="card-body">
-                      <p class="card-text">
-                        الجدول السابق يوضح التمارين الخاصة بك بالخطة الحالية بناء على حالتك الخاصة, يمكنك النقر على أسم التمرين لمشاهدة الفيديو الخاص به, في حال وجود أي تمرين غير مفهوم الرجاء <a href="https://wa.me/message/POU44V4CNULGN1">التواصل معي</a> بشكل مباشر
-                      </p>
-                    </div>
-
-                  </div>
                 </div>
               </div>
         </div>
