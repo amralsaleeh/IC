9cc363830438f8a98967e8528715d7b797867bac
amralsaleeh <3mr.alsaelh@gmail.com>
2023-02-14 15:39:45 +0300

    Main

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -38,7 +38,13 @@
                 <div class="col-lg-4 col-md-12 col-12">
                     <div class="card card-congratulation-medal">
                     <div class="card-body">
-                        <h1>مرحبا مجد</h1>
+                        <h1>
+                            @if (Auth::check())
+                            {{ "مرحبا " . Auth::user()->first_name }}
+                            @else
+                            لم يتم تسجيل الدخول
+                            @endif
+                        </h1>
                         <p class="card-text font-meduim-1">أتمنى لك يوم سعيد</p>
                         <br>
                         <br>
